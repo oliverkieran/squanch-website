557d4f8f9705b76c73b0ebe0a491d17d2bdf0c80
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-09-15 15:58:29 +0200

    added endpoint for chrome extension

---
+++ app/api/commentator/sync/activeCommentators/[event]/route.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse } from "next/server";
+import { useSearchParams } from "next/navigation";
+import prisma from "@/lib/prisma";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { event: string } }
+) {
+  /*
+    This is the endpoint that gets called by the chrome extension.
+    It returns a list of all active commentators for a given event.
+     */
+
+  // Get all active live streams from Mux
+  const auth = Buffer.from(
+    `${process.env.MUX_TOKEN_ID}:${process.env.MUX_TOKEN_SECRET}`
+  ).toString("base64");
+  const muxUrl = "https://api.mux.com/video/v1/live-streams?"; //status=active";
+  const muxResponse = await fetch(muxUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Basic ${auth}`,
+    },
+  });
+
+  if (!muxResponse.ok) {
+    console.error("Mux API call failed: Get Active Live Streams.");
+    return NextResponse.json(
+      { error: "Mux API call failed: Get Active Live Streams." },
+      { status: 500 }
+    );
+  }
+  const { data } = await muxResponse.json();
+  if (data.length > 0) {
+    const liveStreamIds = data.map((stream) => stream.id);
+
+    // Get commentators with active streams and matching event
+    const commentators = await prisma.commentator.findMany({
+      where: {
+        liveStreamId: {
+          in: liveStreamIds,
+        },
+        game: {
+          is: { title: params.event },
+        },
+      },
+      include: {
+        commentator: true,
+        game: true,
+      },
+    });
+
+    return NextResponse.json(commentators);
+  }
+}
