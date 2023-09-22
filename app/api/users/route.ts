e463fd282c1118615693aaab97abf7614a024097
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-09-22 15:52:22 +0200

    removed api endpoint to show users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,28 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET() {
-  const response = await prisma.user.findMany();
-  console.log("RESPONSE:", response);
-  return NextResponse.json(response);
-}
+// export async function GET() {
+//   const response = await prisma.user.findMany();
+//   console.log("RESPONSE:", response);
+//   return NextResponse.json(response);
+// }
 
-// POST /api/users
-export async function POST(req: NextRequest) {
-  console.log("Create User Request received");
-  const { firstname, lastname = "" } = await req.json();
-  const username = `${firstname}${lastname}`.toLowerCase();
-  const email = `${firstname.toLowerCase()}.${lastname.toLowerCase()}@example.com`;
-  const password = "password";
-  const data = {
-    username,
-    firstname,
-    lastname,
-    email,
-    password,
-  };
-  console.log("USER DATA:", data);
-  const response = await prisma.user.create({ data });
-  console.log("RESPONSE:", response);
-  return NextResponse.json(response);
-}
+// // POST /api/users
+// export async function POST(req: NextRequest) {
+//   console.log("Create User Request received");
+//   const { firstname, lastname = "" } = await req.json();
+//   const username = `${firstname}${lastname}`.toLowerCase();
+//   const email = `${firstname.toLowerCase()}.${lastname.toLowerCase()}@example.com`;
+//   const password = "password";
+//   const data = {
+//     username,
+//     firstname,
+//     lastname,
+//     email,
+//     password,
+//   };
+//   console.log("USER DATA:", data);
+//   const response = await prisma.user.create({ data });
+//   console.log("RESPONSE:", response);
+//   return NextResponse.json(response);
+// }
