ddabb1d1b016b4fb8a3391da00905332396b4ee5
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-10-03 12:16:40 +0200

    fixed typo

--- app/(site)/privacy-policy/page.tsx
+++ app/(site)/privacy-policy/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Privacy Policy - Squanch",
-  description: "This is Privacy Policy of Squanch",
+  description: "Privacy Policy of Squanch",
   // other metadata
 };
 
